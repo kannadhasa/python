f659f3e3e41d252fe3f43755f20f98380e4ec71b
Pavila98 <37369661+Pavila98@users.noreply.github.com>
2018-04-12 18:01:48 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -1,5 +1,11 @@
 num=int(input("enter a year"));
-if num%4==0:
-    print("number is leap year")
-elif num%4 != 0:
-    print("number is not a leap year")
+if (num%4==0):
+    if(num%100==0):
+        if(num%400==0):
+            print("number is leap year")
+        else:
+            print("number is not a leap year")
+    else:
+            print("number is not a leap year")
+else:
+            print("number is not a leap year")
